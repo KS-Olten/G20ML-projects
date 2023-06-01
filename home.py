1eb6878f17c242bc87e75e2f2fe5d24d7042abef
JohannesHool <hooljohannes@gmail.com>
2023-06-01 13:15:24 +0200

    Update home.py

--- home.py
+++ home.py
@@ -3,5 +3,5 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 
-st.header('Informatikprojekte G20aM')
+st.header('Informatikprojekte G20ML')
 st.image('https://upload.wikimedia.org/wikipedia/commons/0/02/Computer_science_education.png', width=800)
